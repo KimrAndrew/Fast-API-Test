dc8a4f64bb9e944282e64db9146e7b855fe4cee3
KimrAndrew <andrewrk1997@gmail.com>
2022-04-28 14:22:10 -0700

    edited routes to meet requirements

--- crud_app_one/main.py
+++ crud_app_one/main.py
@@ -1,19 +1,40 @@
-from typing import Optional
+from typing import List
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-@app.get('/employees')
-def read_list():
-    return {"Employee": "List"}
+#Assumes employees can have the same name e.g there can be two unique employees with the name "John Smith"
+@app.post("/employee/", response_model=schemas.Employee)
+def create_employee(employee: schemas.EmployeeCreate, db: Session = Depends(get_db)):
+    return crud.create_employee(db=db, employee=employee)
 
-@app.get("/employees/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+
+@app.get("/employees/", response_model=List[schemas.Employee])
+def read_employees(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    employees = crud.get_employees(db, skip=skip, limit=limit)
+    return employees
+
+
+@app.get("/employees/{employee_id}", response_model=schemas.Employee)
+def read_employee(employee_id: int, db: Session = Depends(get_db)):
+    db_employee = crud.get_employee(db, employee_id=employee_id)
+    if db_employee is None:
+        raise HTTPException(status_code=404, detail="Employee not found")
+    return db_employee
